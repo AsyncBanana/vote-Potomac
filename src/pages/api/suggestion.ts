e6c7d6f8248def6cda80f988af3470a03a0d4108
asyncbanana <jej.jackson.08@gmail.com>
2023-10-22 18:09:41 -0400

    more progress

--- src/pages/api/suggestion.ts
+++ src/pages/api/suggestion.ts
@@ -9,4 +9,28 @@ export const POST: APIRoute = async ({ request, locals }) => {
 			status: 400,
 		});
 	}
+	const title = body.get("title");
+	const description = body.get("description");
+	if (typeof title !== "string" || typeof description !== "string") {
+		return new Response("Please provide a title and description", {
+			status: 400,
+		});
+	}
+	if (
+		title.length < import.meta.env.MIN_TITLE_LENGTH ||
+		title.length > import.meta.env.MAX_TITLE_LENGTH ||
+		description.length < import.meta.env.MIN_DESCRIPTION_LENGTH ||
+		description.length > import.meta.env.MAX_DESCRIPTION_LENGTH
+	) {
+		return new Response(
+			`Please make the title between ${import.meta.env.MIN_TITLE_LENGTH} and ${
+				import.meta.env.MAX_TITLE_LENGTH
+			} characters and the description between ${
+				import.meta.env.MIN_DESCRIPTION_LENGTH
+			} and ${import.meta.env.MAX_DESCRIPTION_LENGTH} characters`,
+			{
+				status: 400,
+			},
+		);
+	}
 };
